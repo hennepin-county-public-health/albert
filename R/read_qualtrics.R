c588e2a509e0fa15184edd26257198f019d0d735
NathanBean-HennepinCountyGov <nathan.bean@hennepin.us>
2025-03-27 10:21:41 -0500

    Updated read_qualtrics with to_snake_case

--- R/read_qualtrics.R
+++ R/read_qualtrics.R
@@ -8,6 +8,6 @@ read_qualtrics <- function(path){
   readr::read_csv(path) %>%
     to_snake_case() %>%
     dplyr::slice(3:nrow(.)) %>%
-    dplyr::select(-c(x1, start_date:ip_address, duration_in_seconds, recipient_email, location_latitude:distribution_channel)) %>%
+    dplyr::select(-c(start_date:ip_address, duration_in_seconds, recipient_email, location_latitude:distribution_channel)) %>%
     dplyr::mutate(dplyr::across(where(is.character), ~stringr::str_replace_all(., "â€™", "'"))) #this is a common error in text imported from Qualtrics
 }
