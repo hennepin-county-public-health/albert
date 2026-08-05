ea9bb29cc6a2563d49cb82bb114628cad57e110e
NathanBean-HennepinCountyGov <52Zc4I9VJYddDWsPkkPk>
2026-08-05 08:14:26 -0500

    typo fix

--- R/compare_cols.R
+++ R/compare_cols.R
@@ -13,7 +13,7 @@ compare_cols <- function(df1, df2){ #if ever necessary, could adjust to compare 
     tidyr::pivot_longer(cols = tidyselect::everything(), values_to = "type1", names_to = "names") |>
     full_join(df2 |>
                 purrr::map(class) |>
-                purrr::list_rbind() |>
+                dplyr::bind_rows() |>
                 tidyr::pivot_longer(cols = tidyselect::everything(), values_to = "type2", names_to = "names"),
               by = "names") #%>%
     #dplyr::filter(type1 != type2) #remove this, can add this behavior afterward
